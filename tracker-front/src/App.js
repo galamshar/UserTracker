66274e9d00ba82ebde6a457637336a639343f292
galamshar <g.myrzagalyev@gmail.com>
2021-07-24 17:21:15 +0600

    Optimized for Azure deployment v2

---
+++ tracker-front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import UsersInfo from './users-info/UsersInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <UsersInfo />
+    </div>
+  );
+}
+
+export default App;
